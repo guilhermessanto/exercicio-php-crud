922b3ec8e51bad3d4fdc4198293ee4c033858ac6
Guilherme Santana <g.santossantana22@gmail.com>
2022-06-03 12:06:58 -0300

    atualização

--- src/conexao.php
+++ src/conexao.php
@@ -1,8 +1,8 @@
 <?php
 $servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "crud_escola_guilherme";
+$usuario = "webmaio1_gui1";
+$senha = "e)NbNogC7ez1";
+$banco = "webmaio1_guilherme1";
 try{
     $conexao = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8",$usuario,$senha);
     $conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
